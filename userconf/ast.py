f5fc0122fa3a0c3a3ad5a647ce5a0b4951bde52a
Andrew <andrewhutchison000@gmail.com>
2020-10-24 15:40:23 +1000

    Implement AST classes

--- userconf/ast.py
+++ userconf/ast.py
@@ -1,2 +1,69 @@
 """Userconf abstract syntax tree data structures and functions.
 """
+from .scan import Token, TokenKind
+
+class AbstractNode:
+    pass
+
+class String(AbstractNode):
+    def __init__(self, data):
+        assert isinstance(data, str)
+        self._data = data
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, data):
+        self._data = data 
+
+class Node(AbstractNode):
+    def __init__(self, /, child_types=AbstractNode):
+        self._children = []
+        self._child_types = child_types
+
+    def add_child(self, child):
+        assert isinstance(child, self._child_types)
+        self._children.append(child)
+
+    @property
+    def children(self):
+        return self._children
+
+class Document(Node):
+    def __init__(self):
+        super().__init__(child_types=RecordItem)
+
+class Record(Node):
+    def __init__(self):
+        super().__init__(child_types=RecordItem)
+
+class Array(Node):
+    def __init__(self):
+        super().__init__(child_types=(Record, String, Array))
+
+class RecordItem(Node):
+    def __init__(self, key, value):
+        super().__init__()
+        assert isinstance(key, String)
+        assert isinstance(value, (Record, Array, String))
+        self._key = key
+        self._value = value
+
+    @property
+    def key(self):
+        return self._key
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        assert isinstance(value, (Record, Array, String))
+        self._value = value
+
+    @property
+    def children(self):
+        return [self.key, self.value]
